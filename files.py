b64ff9c1896fecda944f3c1a9989f91f59d90cef
piotrekcodex <piotrek.codex@gmail.com>
2023-04-28 23:37:27 -0300

    Start XLSX manipulation

--- files.py
+++ files.py
@@ -1,27 +1,48 @@
-import xml.etree.ElementTree as Elements
+from openpyxl import load_workbook
+from openpyxl import Workbook
+import os
 
 
-def read_file():
-    # Open XLSX file
-    opened_file = Elements.parse("results_file/results.xlsx")
+def read_file() -> object:
+    """
 
-    # Get file root
-    file_root = opened_file.getroot()
+    :rtype: object
+    """
+    # Get the current path to set the filename and create a new workbook to importing data
+    filename: str = os.path.join(os.getcwd(), "results_file", "results.xlsx")
+    file_workbook = load_workbook(filename)
 
-    # Get all
-    for child in file_root:
-        # Acesse um atributo específico do elemento
-        print(child.attrib['Apostas'])
+    return file_workbook
 
 
-'''
-# Itere sobre os elementos filhos da raiz
-for child in root:
-    # Acesse um atributo específico do elemento
-    print(child.attrib['atributo'])
+def create_data(file_workbook) -> object:
+    # Create the new workbook, select and rename the active sheet and then save the new workbook
+    new_workbook = Workbook()
+    new_sheet = new_workbook.active
+    new_sheet.title = "Data"
+    new_workbook.save("recurrence.xlsx")
 
-    # Itere sobre os elementos filhos deste elemento
-    for subchild in child:
-        # Acesse o texto do elemento
-        print(subchild.text)
-'''
+    # Access file workbook sheet and checks the total number of draws made
+    file_sheet = file_workbook["Sheet1"]
+    draws_total = file_sheet['A2'].value
+
+    for row in file_sheet.iter_rows(min_row=2, min_col=3, max_col=8, max_row=draws_total, values_only=True):
+        for cell in row:
+            target_cell = "B" + str(cell)
+            print('target')
+            print(new_sheet[target_cell].value)
+            check_cell_value = new_sheet[target_cell].value
+            print(check_cell_value)
+
+            if (str(check_cell_value) == 'None') or (str(check_cell_value) == 'none'):
+                print('entrei')
+                new_sheet[target_cell] = 1
+                check_cell_value2 = new_sheet[target_cell].value
+                print(check_cell_value2)
+            else:
+                print('else')
+                #new_sheet[target_cell] = int(new_sheet[target_cell]) + cell
+                print(new_sheet[target_cell])
+                print('fim')
+
+    return new_workbook
--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-from openpyxl import load_workbook
-import os
+from files import *
 
 
-def read_file() -> object:
-    # Get the current path and set the filename
-    filename: str = os.path.join(os.getcwd(), "results_file", "results.xlsx")
-
-    # Create a new workbook importing data from file
-    # Then access the correct sheet
-    workbook = load_workbook(filename)
-    sheet_ranges = workbook["Sheet1"]
-
-    print(f"Valor do dado:", sheet_ranges['D18'].value)
-
-    # Return
-    return filename
-
-read_file()
+file_workbook = read_file()
+create_data(file_workbook)
